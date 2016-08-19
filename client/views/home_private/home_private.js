2b557193c8f59b7b766596d8c268b234ae55dd8a
AndreyMaxim <andreymaxim@hotmail.com>
2016-08-19 21:26:28 +0300

    new format for denamic list

--- client/views/home_private/home_private.js
+++ client/views/home_private/home_private.js
@@ -48,18 +48,18 @@ Template.HomePrivate.helpers({
 		return (this.preview_count > 0);
 	},
 	getBracketHeight: function(exer_len) {
-		defaultLen = 0.25;
+		defaultLen = 0.3;
 		if(exer_len == 1) {
 			return exer_len + defaultLen;
 		}
 		return parseFloat(exer_len - defaultLen);
 	},
 	centerToBracket: function(exer_len) {
-		heightPerExercise = 24;
-		if(exer_len <= 3) {
+		heightPerExercise = 2;
+		if(exer_len < 3) {
 			return 0;
 		}
-		return ((exer_len / 2) * heightPerExercise);
+		return (((exer_len / 2) - 1) * heightPerExercise);
 	},
 	isSingleCycle: function(cycle) {
 		return (1 == cycle);
